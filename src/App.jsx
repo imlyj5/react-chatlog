1b65abde17bc74c5be8c3459912ceb1d2c3a3f33
Jeslyn Lai <imlyj5@gmail.com>
2025-06-11 22:03:31 -0700

    wave 1 set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,9 +10,15 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry 
+          sender="Vladimir" 
+          body="why are you arguing with me" 
+          timeStamp="2018-05-29T22:49:06+00:00" 
+        />
       </main>
     </div>
   );
 };
 
 export default App;
+
